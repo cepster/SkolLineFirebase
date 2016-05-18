5c3ed2cad059b7e2c85cd2be06c35a2af2aeeeb0
matt.richards <matt.richards@infinitecampus.com>
2016-05-18 17:43:19 -0500

    Added search filter on music

--- src/music/music.js
+++ src/music/music.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {MusicDataService} from '../service/musicDataService';
+import _ from 'underscore';
 
 let musicDataService;
 let router;
@@ -37,5 +38,13 @@ export class Music {
     ]);
 
     this.childRouter = childRouter;
+  }
+}
+
+export class FilterOnNameValueConverter {
+  toView(array, filter) {
+    return _.filter(array, (item) => {
+      return !filter || item.name.toLowerCase().includes(filter.toLowerCase());
+    });
   }
 }
