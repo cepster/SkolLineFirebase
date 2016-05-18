f7ec20b002e2a3d0b106e92afc5e7138d7d82750
matt.richards <matt.richards@infinitecampus.com>
2016-05-18 09:58:12 -0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Skol Line';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
+      {route: 'music',                name: 'music',       moduleId:'music/music',    title:'Music',   nav:true},
+      {route: 'musicDetail/:musicID', name: 'musicDetail', moduleId:'musicDetail'},
+      {route: 'members',              name: 'members',     moduleId:'members/members',  title:'Members', nav:true},
+      {route: 'gigs',                 name: 'gigs',        moduleId:'gigs/gigs',     title:'Gigs',    nav:true},
+      {route: 'gigDetail/:gigID',     name: 'gigDetail',   moduleId:'gigs/gigDetail'},
+      {route: 'social',               name: 'social',      moduleId:'social',   title:'Social',   nav:true}
+    ]);
+
+    /*
+    config.title = 'The Minnesota Vikings Skol Line';
+    config.map([
+      {route: ['', 'home'],           name: 'home',        moduleId:'main',     title:'Home',    nav:true},
+      {route: 'music',                name: 'music',       moduleId:'music/music',    title:'Music',   nav:true},
+      {route: 'musicDetail/:musicID', name: 'musicDetail', moduleId:'musicDetail'},
+      {route: 'members',              name: 'members',     moduleId:'members/members',  title:'Members', nav:true},
+      {route: 'gigs',                 name: 'gigs',        moduleId:'gigs/gigs',     title:'Gigs',    nav:true},
+      {route: 'gigDetail/:gigID',     name: 'gigDetail',   moduleId:'gigs/gigDetail'},
+      {route: 'social',               name: 'social',      moduleId:'social',   title:'Social',   nav:true}
+    ]);
+    */
+    this.router = router;
+  }
+}
